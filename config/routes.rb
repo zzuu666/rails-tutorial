f32cab82a97abf0473a6f74158279b68c7b27ab1
zzuu666 <zzuu666@outlook.com>
2018-03-07 15:07:24 +0800

    Add User Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  # static page router
+  get '/home',         to: 'static_pages#home'
+  get '/help',         to: 'static_pages#help'
+  get '/about',        to: 'static_pages#about'
+  get '/contact',      to: 'static_pages#contact'
+  get '/signup',       to: 'user#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'static_pages#home'
 end
