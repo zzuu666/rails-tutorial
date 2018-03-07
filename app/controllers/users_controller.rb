2df68a7050742170c4ab7b0565f9bef246e4d6b5
zzuu666 <zzuu666@outlook.com>
2018-03-07 16:44:57 +0800

    Add User create show & index

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  # GET /users
+  def index
+    @user = User.all
+  end
+
+  # GET /users/1
+  def show
+    @user = User.find(params[:id])
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to Sample App."
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   get '/help',         to: 'static_pages#help'
   get '/about',        to: 'static_pages#about'
   get '/contact',      to: 'static_pages#contact'
-  get '/signup',       to: 'user#new'
+  get '/signup',       to: 'users#new'
+  post '/signup',      to: 'users#create'
+
+
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
