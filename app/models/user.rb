f716cfa01489b06765fdf34d2ce4b9f8c9c208a1
zzuu666 <zzuu666@outlook.com>
2018-04-02 16:12:54 +0800

    Finish Rails tutorial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships,
+           class_name: "Relationship",
+           foreign_key: "follower_id",
+           dependent: :destroy
+  has_many :passive_relationships,
+           class_name: "Relationship",
+           foreign_key: "followed_id",
+           dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
@@ -65,8 +75,27 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  # 信息流
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
+
+  # 关注
+  def follow(other_user)
+    following << other_user
+  end
+
+  # 取关
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # 是否关注了谁
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
